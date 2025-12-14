195cb698dc1b397570011b0f0f1f014363ba019e
Kunal Gupta <k5gupta@ucsd.edu>
2025-12-14 07:08:35 +0530

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="sceneprogexec",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),  # Automatically finds the 'sceneprogexec' package (and any sub-packages)
     entry_points={
         "console_scripts": [
